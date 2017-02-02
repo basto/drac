d4e9e208bb68836a36afe4b0ca77f52af74c3b42
Abdulmi <aalmetwa@uwaterloo.ca>
2017-02-01 23:43:19 -0500

    added file structure

--- src/components/app.js
+++ src/components/app.js
@@ -1,22 +1,10 @@
 import React from 'react';
-import TodosList from './todos-list'
-const todos = [
-  {
-    task: 'make React',
-    isCompleted: false
-  },
-  {
-    task: 'do laundry',
-    isCompleted: true
-  }
-]
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
-        <h1> React Todos App </h1>
-        <TodosList />
+        <h1> React App Works</h1>
       </div>
     );
   }
